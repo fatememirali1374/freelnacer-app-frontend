ae4c36fda05ce84a98a129200334b3c84004f527
fateme Mirali <fatememirali1374@gmail.com>
2024-02-19 09:20:34 +0330

    config app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <h1 className="text-3xl text-primary-800 font-bold bg-secondary-200 container">
+    شروع پروژه
+  </h1>
+  )
+}
+
+export default App
