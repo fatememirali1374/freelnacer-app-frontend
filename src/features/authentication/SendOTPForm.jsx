e5ab8c1ab267bbc821054c083ac547c88a8445ce
fateme Mirali <fatememirali1374@gmail.com>
2024-02-20 08:57:01 +0330

    create check OTP ui

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import TextField from "../../ui/TextField"
 
 
 function SendOTPForm() {
@@ -7,19 +8,13 @@ function SendOTPForm() {
         <div>
             <form className=" space-y-8">
                 <div>
-                    <label className=" mb-1" htmlFor="phonenumber">شماره موبایل</label>
-                    <input
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                        id="phonenumber"
-                        className=" w-full py-3 px-4 rounded-xl text-secondary-900 border border-gray-300 
-                        hover:border-primary-500 focus:border-primary-500 
-                        focus:bg-white transition-all duration-300 ease-in-out
-                        focus:shadow-lg focus:shadow-primary-200"
-                        type="text" />
+                    <TextField name="phoneNumber" 
+                    value={phoneNumber}
+                    label=" شماره موبایل"
+                    onChange={(e)=>setPhoneNumber(e.target.value)} 
+                     />
                 </div>
-                <button className=" px-4 py-2 font-bold bg-primary-900 text-secondary-100 w-full rounded-xl
-               transition-all duration-300 hover:bg-primary-800 hover:shadow-xl shadow-primary-300 ">ارسال کد تایید</button>
+                <button className=" btn btn--primary w-full ">ارسال کد تایید</button>
             </form>
         </div>
     )
