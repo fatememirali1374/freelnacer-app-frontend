31d35acbd53974774e43313f36bd46ac17b66605
fateme Mirali <fatememirali1374@gmail.com>
2024-02-21 08:22:22 +0330

    add step and loadin component

---
+++ src/features/authentication/AuthContiner.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import CheckOTPForm from "./CheckOTPForm"
+import SendOTPForm from "./SendOTPForm"
+
+function AuthContiner() {
+    const [step,setStep]= useState(1)
+
+    const renderStep=()=>{
+        switch(step){
+case 1: 
+return <SendOTPForm setStep={setStep}/>;
+case 2:
+     return <CheckOTPForm/>;
+default:
+     return null
+        }
+    }
+  return (
+    <div className=" w-full sm:max-w-sm">
+{renderStep()}
+    </div>
+  )
+}
+
+export default AuthContiner
--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -3,34 +3,38 @@ import TextField from "../../ui/TextField"
 import { useMutation } from "@tanstack/react-query"
 import { getOtp } from "../../services/authService"
 import toast from "react-hot-toast"
+import Loading from "../../ui/Loading"
 
 
-function SendOTPForm() {
+function SendOTPForm({ setStep }) {
     const [phoneNumber, setPhoneNumber] = useState("")
-    const {isPending, error, data, mutateAsync}= useMutation({
+    const { isPending, error, data, mutateAsync } = useMutation({
         mutationFn: getOtp,
     })
-    const sendOtpHandler=async (e)=>{
-e.preventDefault();
-try {
-    const data= await mutateAsync({phoneNumber})
-    toast.success(data.message);
-} catch (error) {
-    toast.error(error?.response?.data?.message)
-}
+    const sendOtpHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const data = await mutateAsync({ phoneNumber })
+            toast.success(data.message);
+            setStep(2)
+        } catch (error) {
+            toast.error(error?.response?.data?.message)
+        }
     }
-       return (
+    return (
         <div>
             <form className=" space-y-8" onSubmit={sendOtpHandler}>
                 <div>
-                    <TextField name="phoneNumber" 
-                    value={phoneNumber}
-                    label=" شماره موبایل"
-                    onChange={(e)=>setPhoneNumber(e.target.value)} 
-                     />
+                    <TextField name="phoneNumber"
+                        value={phoneNumber}
+                        label=" شماره موبایل"
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
                 </div>
-                <button type="submit" className=" btn btn--primary w-full ">ارسال کد تایید</button>
-            </form>
+                {isPending ? (<Loading/>) : (
+                    <button type="submit" className=" btn btn--primary w-full ">ارسال کد تایید</button>
+                )
+                } </form>
         </div>
     )
 }
